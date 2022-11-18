65c59d011dbf8f9b79803964b3d93ceac2cc5604
Blazej-oWo <b.wojtusciszyn@gmail.com>
2022-11-18 11:41:09 +0100

    adding script in js file, for mobile menu

---
+++ js/script.js
@@ -0,0 +1,4 @@
+const menuMobile = document.querySelector('#menu-mobile');
+const overlay = document.querySelector('#overlay');
+const showMenu = document.querySelector('#show-menu');
+const hideMenu = document.querySelector('#hide-menu');
